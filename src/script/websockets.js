7aebe1ad1d34d2155b997ece37a9bdd16c581e3d
Steedlan <40958752+Steedlan@users.noreply.github.com>
2023-12-01 16:12:19 +0100

    wss

--- src/script/websockets.js
+++ src/script/websockets.js
@@ -9,7 +9,7 @@ const { setLoadingBarPercentage, afficherChargement, afficherInformation, stopAf
 const { updatePlayer, removeCard, imageUno, endGame } = require('./game');
 const { generateChatBox, addMessage } = require('./chat');
 
-const link = 'ws://155.248.239.223:25568';
+const link = 'wss://155.248.239.223:25568';
 
 // const link = 'ws://localhost:25568';
 
